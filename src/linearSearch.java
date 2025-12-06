97e98b6948741a628ad214ff8668c638bcf829dc
berkee25 <berkeozdemir25@gmail.com>
2025-12-06 17:34:57 +0300

    Update linearSearch.java

--- src/linearSearch.java
+++ src/linearSearch.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
-public class linearSearch {
+class linearSearch {
+    int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
