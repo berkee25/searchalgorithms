5b0d1b6a4f978aa70b36828642ac9d3a78b2ed8f
berkee25 <berkeozdemir25@gmail.com>
2025-12-06 17:35:08 +0300

    Update recursiveLinearSearch.java

--- src/recursiveLinearSearch.java
+++ src/recursiveLinearSearch.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class recursiveLinearSearch {
+    // Recursive
+    int linearSearchRecursive(int[] arr, int target, int index) {
+
+
+        if (index == arr.length) {
+            return -1;
+        }
+
+
+        if (arr[index] == target) {
+            return index;
+        }
+
+
+        return linearSearchRecursive(arr, target, index + 1);
+    }
 }
+
